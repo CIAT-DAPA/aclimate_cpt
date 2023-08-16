712c372d05004b4fbad3bd6e34ab2d52a6247dca
santiago123x <santiago123x@users.noreply.github.com>
2023-08-16 15:29:34 +0000

    Update version to v0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aclimate_cpt",
-    version='v0.0.4',
+    version='v0.0.5',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="Prediction module",
